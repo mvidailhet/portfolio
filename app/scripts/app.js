7c085e24bc5da7d0ec04110a14ac3827f63ea5be
mvidailhet <mvidailhet@mncc.fr>
2015-06-01 16:00:37 -0700

    Yeoman Angular Base

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portfolioApp
+ * @description
+ * # portfolioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('portfolioApp', []);
